b9e6682633ed2a29c55f18f1aab9345717af5dba
Ammorishita <57607434+AesisGaming@users.noreply.github.com>
2021-10-24 12:30:53 -0500

    Updates

---
+++ precache-manifest.5fa07e018f4afa79561f9b2debb83137.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53c715c430bb5cd67380ff2b160dcf9e",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "29b4557b1d3ed6e8ae31",
+    "url": "/my-app/static/css/main.d608c3cf.chunk.css"
+  },
+  {
+    "revision": "a02967c989f45e4b3fa4",
+    "url": "/my-app/static/js/2.3f585b7d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.3f585b7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29b4557b1d3ed6e8ae31",
+    "url": "/my-app/static/js/main.109a56bd.chunk.js"
+  },
+  {
+    "revision": "68584af38886ab1c2b14",
+    "url": "/my-app/static/js/runtime-main.4a721b49.js"
+  },
+  {
+    "revision": "838d56d641b629fe6fd6ac030e3c4ef5",
+    "url": "/my-app/static/media/flash-sprite-2.838d56d6.png"
+  }
+]);
